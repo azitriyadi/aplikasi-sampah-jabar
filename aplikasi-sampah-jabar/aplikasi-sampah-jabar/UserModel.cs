673ab4a63eba083afec6c7ac22b4a53e75a6e21e
azitriyadi <158010916+azitriyadi@users.noreply.github.com>
2026-01-02 21:44:36 +0700

    BE login dan regis done

---
+++ aplikasi-sampah-jabar/aplikasi-sampah-jabar/UserModel.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace aplikasi_sampah_jabar
+{
+    public class UserModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("username")]
+        public string Username { get; set; }
+
+        [BsonElement("password")]
+        public string Password { get; set; } // Disimpan dalam bentuk ter-enkripsi (Hash)
+
+        [BsonElement("role")]
+        public string Role { get; set; } // Contoh: "admin", "petugas"
+
+        [BsonElement("created_at")]
+        public DateTime CreatedAt { get; set; }
+    }
+}
