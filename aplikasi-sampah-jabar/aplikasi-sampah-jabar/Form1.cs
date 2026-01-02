c5fae01de6b10a1788a1c0991c05ba6babab38e5
azitriyadi <158010916+azitriyadi@users.noreply.github.com>
2026-01-02 18:33:05 +0700

    koneksi database done

--- aplikasi-sampah-jabar/aplikasi-sampah-jabar/Form1.cs
+++ aplikasi-sampah-jabar/aplikasi-sampah-jabar/Form1.cs
@@ -6,5 +6,30 @@ namespace aplikasi_sampah_jabar
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 1. Panggil class MongoHelper
+                var db = MongoHelper.GetDatabase();
+
+                // 2. Ambil nama database untuk membuktikan koneksi
+                string namaDatabase = db.DatabaseNamespace.DatabaseName;
+
+                // 3. Tampilkan pesan sukses
+                MessageBox.Show("ALHAMDULILLAH! \nKoneksi Sukses ke: " + namaDatabase, "Info Koneksi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                // Jika gagal, tampilkan pesan error
+                MessageBox.Show("GAGAL KONEK: \n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
